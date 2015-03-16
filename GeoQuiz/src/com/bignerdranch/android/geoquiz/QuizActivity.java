383cca01ea3061d0c2a278a8f98548995d36db02
BMamba2942 <jmiddleton9140@sbcglobal.net>
2015-03-15 23:33:40 -0500

    Compiling and Running

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,8 +1,8 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class QuizActivity extends ActionBarActivity {
+public class QuizActivity extends Activity {
 	private static final String TAG = "QuizActivity";
 	private static final String KEY_INDEX = "index";
 	private Button mTrueButton;
